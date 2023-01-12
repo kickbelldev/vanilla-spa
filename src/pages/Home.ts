9f658301f876c79b7ca8953c7371d259733d3f7f
zellypish <ehxhfl21@gmail.com>
2023-01-12 23:34:51 +0900

    refactor: PostList 페이지 => Home 페이지로 변경

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -1,22 +1,48 @@
-import Component, { PropsType, StateType } from '../Component'
+import { AxiosError } from 'axios'
+import Component, { PropsType } from '../Component'
 import Header from '../components/Header'
-import Hello from '../components/Hello'
+import PostList from '../components/PostList'
+import { PostListType } from '../types/Post'
+import { PostListRes, Response } from '../types/Response'
+import fetch from '../utils/fetch'
 
-class Home extends Component<PropsType, StateType> {
+interface PostListStateType {
+  list: PostListType
+}
+
+class Home extends Component<PostListStateType, PropsType> {
   didMount(): void {
     const $header: Element = this.target.querySelector('header')!
     new Header($header, {})
 
-    const $hello: Element = this.target.querySelector('.hello')!
-    new Hello($hello, { hello: 'World' })
+    fetch
+      .get<Response<PostListRes>>('/posts')
+      .then(({ data: res }) => {
+        console.log(res)
+        this.setState({ list: res.data.posts })
+      })
+      .catch((err: AxiosError) => {
+        if (err.response) {
+          window.alert(`로딩에 실패했습니다. 에러코드: ${err.response.status}`)
+        }
+      })
+  }
+
+  didUpdate(): void {
+    const $header: Element = this.target.querySelector('header')!
+    new Header($header, {})
+
+    const $postList: Element = this.target.querySelector('.post-list')!
+    new PostList($postList, { list: this.state.list })
   }
 
   template(): string {
     return `
     <div>
       <header></header>
-      <div class="hello"></div>
-    </div>`
+      <div class="post-list"></div>
+    </div>
+    `
   }
 }
 
